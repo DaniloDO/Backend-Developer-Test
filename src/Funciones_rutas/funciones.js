88b84fa9f2e70723351b476775186cfce78e983f
Danilo Donado <danilodonadoa@gmail.com>
2022-08-18 23:58:26 -0400

    Actualizacion funciones CRUD

--- src/Funciones_rutas/funciones.js
+++ src/Funciones_rutas/funciones.js
@@ -5,12 +5,14 @@ export const createFunction = (req, res) => {
     let body = req.body;
     let {titulo, autor, descripcionGrafica, contenido} = req.body;
     let {imagen, categoria, fechaPublicacion, usuario} = req.body;
-    //res.send(body)
 
     req.getConnection((err, connection) => {
-        if(err) return next(err);
+        if(err) {
+            res.send(err);
+            return next(err);
+        }
         connection.query(`INSERT INTO Post (Titulo, Autor, DescripcionGrafica, Contenido, Imagen, Categoria, FechaPublicacion, Usuario) VALUES ('${titulo}', '${autor}', '${descripcionGrafica}', '${contenido}', '${imagen}', '${categoria}', '${fechaPublicacion}', '${usuario}')`, (err, data) =>{
-            res.send(data)
+            res.json(data)
         });
     });
 };
@@ -18,10 +20,12 @@ export const createFunction = (req, res) => {
 //Leer blogs en base de datos
 export const readFunction = (req, res, next) => {
     let titulo = req.params.titulo;
-    //res.send(titulo);
 
     req.getConnection((err, connection) => {
-        if(err) return next(err);
+        if(err) {
+            res.send(err)
+            return next(err);
+        }
         connection.query(`SELECT ${titulo} FROM Post`, (err, data) =>{
             res.send(data)
         });
@@ -32,26 +36,30 @@ export const readFunction = (req, res, next) => {
 export const updateFunction = (req, res) => {
     let titulo = req.params.titulo;
     let body = req.body;
-    res.send(body)
 
-    /*req.getConnection((err, connection) => {
-        if(err) return next(err);
-        connection.query('', (err, data) =>{
+    req.getConnection((err, connection) => {
+        if(err) {
+            res.send(err)
+            return next(err);
+        }
+        connection.query(`UPDATE Post SET Autor = '${autor}', DescripcionGrafica = '${descripcionGrafica}', Contenido = '${contenido}', Imagen = '${imagen}', Categoria = '${categoria}', FechaPublicacion = '${fechaPublicacion}', Usuario = '${usuario}' WHERE ${titulo}`, (err, data) =>{
             res.send(data)
         });
-    });*/
+    });
  
 };
 
 //Borrar blogs en base de datos
 export const deleteFunction = (req, res) => {
     let titulo = req.params.titulo;
-    res.send(titulo);
 
-    /*req.getConnection((err, connection) => {
-        if(err) return next(err);
-        connection.query('', (err, data) =>{
+    req.getConnection((err, connection) => {
+        if(err){
+            res.send(err);
+            return next(err);
+        }
+        connection.query(`DELETE FROM Post WHERE ${titulo}`, (err, data) =>{
             res.send(data)
         });
-    });*/    
+    });  
 };
